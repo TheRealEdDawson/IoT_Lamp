b3c903e0248f61bd7be39542eb86b023b411a71e
Ryan Cather <rytanc@me.com>
2015-01-19 16:19:11 +1100

    specific on/off cpp commands and updated script
    It works! Got specific cpp scripts to turn the LED on and off.
    The Python script calls the compiled cpp scripts in a specific folder
    so needs to be updated if deploying on another Pi.

---
+++ Working RF Lamp/weblamp1.py
@@ -0,0 +1,86 @@
+import RPi.GPIO as GPIO
+import os
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+GPIO.setmode(GPIO.BCM)
+#GPIO.setmode(GPIO.BOARD)
+
+state = False
+
+# Create a dictionary called pin to store the pin number, name and pin state: 
+# 24 : {'name' : 'coffee maker', 'state' : GPIO.LOW},
+
+pins = {
+	23 : {'name' : 'lamp', 'state' : GPIO.LOW}
+	}
+
+#Set each pin as an outpuit and make it low:
+for pin in pins:
+	GPIO.setup(pin, GPIO.OUT)
+	GPIO.output(pin, GPIO.LOW)
+
+@app.route("/")
+def main():
+	#For each pin, read the pin state and store it in the pins dictionary:
+	for pin in pins:
+	#	pins[pin]['state'] = GPIO.input(pin)
+		print pin
+	# Put the pin dictionary into the template data dictionary:
+	templateData = {
+		'pins' : pins
+		}
+	# Pass the template data into the tempalte main.html and return it to the user
+	return render_template('main.html', **templateData)
+	
+
+# The function belopw is executed when someone requests a URL with the pin number and action in it:
+@app.route("/<changePin>/<action>")
+def action(changePin, action):
+	# Convert the pin from the URL into an integer:
+	changePin = int(changePin)
+	# GPIO.setup(changePin, GPIO.OUT)
+	# Get the device nasme for the pin being changed:
+	deviceName = pins[changePin]['name']
+	# If the action part of the URL is "on", execute the code indented below:
+	if action == "on":
+		# Set the pin high:
+		#GPIO.output(changePin, GPIO.HIGH)
+		os.system("sudo /home/pi/iot_lamp/rf24libs/RF24/examples_RPi/turnLEDOn")
+		# Save the status message to be passed into the template:
+		message = "Turned " + deviceName + " on."
+		state = True
+	if action == "off":
+		# GPIO.output(changePin, GPIO.LOW)
+		os.system("sudo /home/pi/iot_lamp/rf24libs/RF24/examples_RPi/turnLEDOff")
+
+		message = "Turned " + deviceName + " off."
+		state = False
+	if action == "toggle":
+		# Read the pin and set it to whatever it isn't, i.e. toggle it
+		print "Priting Changepin"
+		#print GPIO.gpio_function(changePin)
+		#GPIO.output(changePin, not GPIO.input(changePin))
+		GPIO.setup(changePin, GPIO.IN)
+		tempValue =  GPIO.input(changePin)
+		GPIO.setup(changePin, GPIO.OUT)
+		GPIO.output(changePin, not tempValue)
+		message = "Toggled " + deviceName + "."
+		state = not state
+
+	# For each pin, read the pin state and store it in the pins dictionary:
+	#for pin in pins:
+	#	pins[pin]['state'] = GPIO.input(pin)
+		
+	# Along with the pin dictionary, put the message into the template data dictionary:
+	templateData = {
+		'message' : message,
+		'pins' : pins,
+		'state' : state
+		}
+
+	return render_template('main.html', **templateData)
+
+if __name__ == "__main__":
+	app.run(host='0.0.0.0', port=80, debug=True)
+
